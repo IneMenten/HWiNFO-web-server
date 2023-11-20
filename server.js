445c5613934afcb84b1f4567f72c79d82b1f106a
IneMenten <inementen@gmail.com>
2023-11-20 20:06:43 +0100

    Create endpoint for HWiNFO data

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+var exec = require('child_process').exec;
+const app = express();
+const port = 8085;
+
+app.get('/', (_req, res) => {
+	exec(
+		`reg query HKEY_CURRENT_USER\\SOFTWARE\\HWiNFO64\\VSB`,
+		function (_error, stdout, _stderror) {
+			let data = stdout.replaceAll('REG_SZ', '').split('\r\n').slice(2, -2);
+			const propertyCount = 5;
+			let json = '{"hwinfo": [';
+			for (let i = 0; i < data.length; i += propertyCount) {
+				const properties = data.slice(i, i + propertyCount);
+				let innerJson = '{';
+				properties.forEach(element => {
+					const trimmedElement = element
+						.trim()
+						.replace('        ', ': ')
+						.split(': ');
+					const key = trimmedElement[0].replace(/[0-9]/g, '');
+					const value = trimmedElement[1]
+						.replace(' �C', '°C')
+						.replace(' %', '%')
+						.replace(',', '')
+						.replace('.', ',');
+					if (element == properties.slice(-1)) {
+						innerJson += `"${key}": "${value}"`;
+					} else {
+						innerJson += `"${key}": "${value}",`;
+					}
+				});
+				if (i == data.length - properties) {
+					innerJson += '}';
+				} else {
+					innerJson += '},';
+				}
+				json += innerJson;
+			}
+			json += ']}';
+			res.status(200).set('Content-Type', 'application/json').send(json);
+		}
+	);
+});
+
+app.get('/kill', (_req, _res) => {
+	setTimeout(() => process.exit(), 500);
+});
+
+app.use((_req, res, next) => {
+	res.status(404).send('404 Page Not Found');
+});
+
+app.listen(port, function () {
+	console.log('Application started on ' + new Date());
+	console.log('Listening on ' + port);
+});
